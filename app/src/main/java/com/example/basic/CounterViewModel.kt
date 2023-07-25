4f56c6ce41ff8795678f0a4bc832dbef37243d14
Soma Sekine <s.sekine.howcollect@gmail.com>
2023-07-26 00:10:00 +0900

    CounterViewModelを追加

---
+++ app/src/main/java/com/example/basic/CounterViewModel.kt
@@ -0,0 +1,15 @@
+package com.example.basic
+
+import androidx.lifecycle.ViewModel
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.asStateFlow
+
+class CounterViewModel : ViewModel() {
+    private val _count: MutableStateFlow<Int> = MutableStateFlow(0)
+    val count: StateFlow<Int> = _count.asStateFlow()
+
+    fun increaseCount() {
+        _count.value++
+    }
+}
