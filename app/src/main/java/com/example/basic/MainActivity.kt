ddcfe167535f4016b42cc025cd78bd3b0efcfcc0
Soma Sekine <s.sekine.howcollect@gmail.com>
2023-07-25 22:40:06 +0900

    ImageSampleでModifierを使って画像の表示スタイルを変更

--- app/src/main/java/com/example/basic/MainActivity.kt
+++ app/src/main/java/com/example/basic/MainActivity.kt
@@ -11,6 +11,8 @@ import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.CheckCircle
 import androidx.compose.material3.Button
@@ -28,7 +30,9 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
@@ -48,10 +52,15 @@ class MainActivity : ComponentActivity() {
                 ) {
 //                    Greeting("Android")
 //                    SimpleFilledTextFieldSample()
+//                    Box(
+//                        contentAlignment = Alignment.Center
+//                    ) {
+//                        ButtonSample()
+//                    }
                     Box(
                         contentAlignment = Alignment.Center
                     ) {
-                        ButtonSample()
+                        ImageSample()
                     }
                 }
             }
@@ -63,10 +72,13 @@ class MainActivity : ComponentActivity() {
 fun ImageSample() {
     val imageModifier = Modifier
         .size(200.dp)
-        .border(BorderStroke(1.dp, Color.Black))
+        .clip(CircleShape)
+//        .clip(RoundedCornerShape(24.dp))
     Image(
         painter = painterResource(id = R.drawable.sample_image),
-        contentDescription = "サンプル画像　ダックスフンド"
+        contentDescription = "サンプル画像　ダックスフンド",
+        modifier = imageModifier,
+        contentScale = ContentScale.Crop,
     )
 }
 
