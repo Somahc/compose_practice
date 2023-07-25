7d94f59a41b849a122153dbc1034649383fbcebe
Soma Sekine <s.sekine.howcollect@gmail.com>
2023-07-25 22:16:11 +0900

    Boxを使ってボタンのサイズを調整

--- app/src/main/java/com/example/basic/MainActivity.kt
+++ app/src/main/java/com/example/basic/MainActivity.kt
@@ -5,10 +5,16 @@ import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.size
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.CheckCircle
 import androidx.compose.material3.Button
 import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
@@ -18,6 +24,7 @@ import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.stringResource
@@ -36,9 +43,13 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Greeting("Android")
-                    SimpleFilledTextFieldSample()
-                    ButtonSample()
+//                    Greeting("Android")
+//                    SimpleFilledTextFieldSample()
+                    Box(
+                        contentAlignment = Alignment.Center
+                    ) {
+                        ButtonSample()
+                    }
                 }
             }
         }
@@ -49,13 +60,19 @@ class MainActivity : ComponentActivity() {
 fun ButtonSample() {
     Button(
         onClick = { Log.d("Button", "Clicked") },
-            shape = MaterialTheme.shapes.extraSmall,
-            border = BorderStroke(2.dp, Color.DarkGray),
-            colors = ButtonDefaults.buttonColors(
-                containerColor = Color.Green,
-                contentColor = Color.Black
-            )
+//            shape = MaterialTheme.shapes.extraSmall,
+//            border = BorderStroke(2.dp, Color.DarkGray),
+//            colors = ButtonDefaults.buttonColors(
+//                containerColor = Color.Green,
+//                contentColor = Color.Black
+//            )
     ){
+        Icon(
+            Icons.Filled.CheckCircle,
+            contentDescription = null,
+            modifier = Modifier.size(ButtonDefaults.IconSize)
+        )
+        Spacer(Modifier.size(ButtonDefaults.IconSpacing))
         Text("Tap Button")
     }
 }
