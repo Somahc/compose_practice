6bac14d0d455ebe0d9b4efae5155ce8491d8d161
Soma Sekine <s.sekine.howcollect@gmail.com>
2023-07-25 21:58:05 +0900

    SampleButton関数を定義

--- app/src/main/java/com/example/basic/MainActivity.kt
+++ app/src/main/java/com/example/basic/MainActivity.kt
@@ -1,9 +1,13 @@
 package com.example.basic
 
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
@@ -18,6 +22,7 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.example.basic.ui.theme.BasicTheme
 
@@ -33,10 +38,32 @@ class MainActivity : ComponentActivity() {
                 ) {
                     Greeting("Android")
                     SimpleFilledTextFieldSample()
+                    ButtonSample()
                 }
             }
         }
     }
+}
+
+@Composable
+fun ButtonSample() {
+    Button(
+        onClick = { Log.d("Button", "Clicked") },
+            shape = MaterialTheme.shapes.extraSmall,
+            border = BorderStroke(2.dp, Color.DarkGray),
+            colors = ButtonDefaults.buttonColors(
+                containerColor = Color.Green,
+                contentColor = Color.Black
+            )
+    ){
+        Text("Tap Button")
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun PreviewButtonSample() {
+    ButtonSample()
 }
 
 @Composable
